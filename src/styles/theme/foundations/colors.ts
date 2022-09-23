2831be4e0ddfd8ebca25a78e15d74907ad068dbb
ChyunKim <co1995@naver.com>
2022-09-23 23:06:19 +0900

    chore: Color setting

--- src/styles/theme/foundations/colors.ts
+++ src/styles/theme/foundations/colors.ts
@@ -1,17 +1,17 @@
 import { getColorSchema } from '@utils/color-generator';
 
 const Light = {
-  primary: getColorSchema('#4850FF'),
-  secondary: getColorSchema('#7B61FF'),
+  primary: getColorSchema('#FF710B'),
+  secondary: getColorSchema('#FFAB2E'),
   warning: getColorSchema('#FF6060'),
-  success: getColorSchema('#4850FF'),
+  success: getColorSchema('#FF710B'),
   gray: {
-    50: '#FAFAFA',
-    100: '#F7F7F7',
+    50: '#FDFDFD',
+    100: '#F9F9F9',
     200: '#F2F3F4',
-    300: '#E5E7EC',
-    400: '#D1D4DD',
-    500: '#B8BCC8',
+    300: '#EAECF0',
+    400: '#CBCED6',
+    500: '#A8ADBA',
     600: '#8C919F',
     700: '#757983',
     800: '#4A4D55',
@@ -21,28 +21,28 @@ const Light = {
   white: '#FFFFFF',
   background: {
     primary: '#FFFFFF',
-    secondary: '#FAFAFA',
-    tertiary: '#F0F0F0',
+    secondary: '#F9F9F9',
+    tertiary: '#F2F3F4',
   },
   modal: '#FFFFFF',
 };
 
 const Dark = {
-  primary: getColorSchema('#485BFF'),
-  secondary: getColorSchema('#755AFF'),
+  primary: getColorSchema('#FF710B'),
+  secondary: getColorSchema('#FFAB2E'),
   warning: getColorSchema('#FF6060'),
-  success: getColorSchema('#4850FF'),
+  success: getColorSchema('#FF710B'),
   gray: {
-    50: '#292A2E',
-    100: '#313236',
-    200: '#39393D',
-    300: '#4A4D55',
-    400: '#5C606A',
-    500: '#757983',
+    50: '#FDFDFD',
+    100: '#F9F9F9',
+    200: '#F2F3F4',
+    300: '#EAECF0',
+    400: '#CBCED6',
+    500: '#A8ADBA',
     600: '#8C919F',
-    700: '#B8BCC8',
-    800: '#D1D4DD',
-    900: '#DFE1E7',
+    700: '#757983',
+    800: '#4A4D55',
+    900: '#292A2E',
   },
   black: '#E5E7EC',
   white: '#232325',
@@ -83,8 +83,8 @@ const Brand = {
 };
 
 const Dim = {
-  primary: '#1A1A1A80',
-  secondary: '#1A1A1A33',
+  primary: '#1A1A1A50',
+  secondary: '#1A1A1A20 ',
 };
 
 const mode = {
