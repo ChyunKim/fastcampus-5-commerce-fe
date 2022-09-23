29a3cffd5c7b3b49dc82b64243483b47f3929c44
ChyunKim <co1995@naver.com>
2022-09-23 23:53:49 +0900

    feat(Signup-Page): Publishing Signup page #2
    Customize Heading

--- src/components/SignupPage/SignupPage.tsx
+++ src/components/SignupPage/SignupPage.tsx
@@ -1,5 +1,16 @@
+import { Box, Container, Heading } from '@chakra-ui/react';
+
+import { LogoIcon } from 'generated/icons/MyIcons';
+
 const SignupPage = () => {
-  return <>SignupPage</>;
+  return (
+    <Container variant="white">
+      <Box m="32px 0">
+        <LogoIcon w="120" color="primary.500" />
+      </Box>
+      <Heading variant="extraTitle">회원가입</Heading>
+    </Container>
+  );
 };
 
 export default SignupPage;
