a53854ce7cece6b842b3dde7da8a2d9645d9fa8b
ChyunKim <co1995@naver.com>
2022-09-24 00:52:52 +0900

    feat(Form-fragment): Validate Form (using yup) #2

--- src/components/SignupPage/SignupPage.tsx
+++ src/components/SignupPage/SignupPage.tsx
@@ -1,5 +1,7 @@
 import { Box, Container, Heading } from '@chakra-ui/react';
 
+import FormPage from './_fragments/FormPage';
+
 import { LogoIcon } from 'generated/icons/MyIcons';
 
 const SignupPage = () => {
@@ -9,6 +11,7 @@ const SignupPage = () => {
         <LogoIcon w="120" color="primary.500" />
       </Box>
       <Heading variant="extraTitle">회원가입</Heading>
+      <FormPage />
     </Container>
   );
 };
---
+++ src/components/SignupPage/_fragments/FormPage.tsx
@@ -0,0 +1,15 @@
+import useSignForm from '../_hooks/useSignForm';
+import FormView from './Formview';
+
+const FormPage = () => {
+  const formData = useSignForm();
+  const { handleSubmit } = formData;
+  const onSubmit = handleSubmit(
+    ({ username, nickname, phone, email, gender, age }) => {
+      console.log(`form 정보: ${username}, ${nickname}, ${phone}, ${email}`);
+    },
+  );
+  return <FormView formData={formData} onSubmit={onSubmit} />;
+};
+
+export default FormPage;
