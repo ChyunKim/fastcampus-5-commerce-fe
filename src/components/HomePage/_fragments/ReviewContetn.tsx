1eb6953b09e17da69796acf71bfed17eefc5dff1
ChyunKim <co1995@naver.com>
2022-09-26 18:44:27 +0900

    chore(star-icon):Import star Icon #4

--- src/components/HomePage/_fragments/ReviewContetn.tsx
+++ src/components/HomePage/_fragments/ReviewContetn.tsx
@@ -8,8 +8,8 @@ const ReviewContent = () => {
         <br />
         고객님의 솔직한 리뷰
       </Heading>
-      <Flex>
-        <Button variant="orange" w="53px" h="30px" fontSize="12px">
+      <Flex mt="50px" mb="70px">
+        <Button variant="orange" w="53px" h="30px" fontSize="12px" m="0 5px">
           전체
         </Button>
         <Button
@@ -18,19 +18,53 @@ const ReviewContent = () => {
           h="30px"
           fontSize="12px"
           bg="gray.200"
+          m="0 5px"
+          fontWeight="400"
         >
           바스&샴푸
         </Button>
-        <Button variant="orange" w="53px" h="30px" fontSize="12px">
+        <Button
+          borderRadius="15px"
+          w="53px"
+          h="30px"
+          fontSize="12px"
+          m="0 5px"
+          fontWeight="400"
+        >
           오일
         </Button>
-        <Button variant="orange" w="53px" h="30px" fontSize="12px">
+        <Button
+          borderRadius="15px"
+          w="53px"
+          h="30px"
+          fontSize="12px"
+          m="0 5px"
+          fontWeight="400"
+        >
           로션
         </Button>
-        <Button variant="orange" w="53px" h="30px" fontSize="12px">
+        <Button
+          borderRadius="15px"
+          w="53px"
+          h="30px"
+          fontSize="12px"
+          m="0 5px"
+          fontWeight="400"
+        >
           크림
         </Button>
       </Flex>
+      <Box
+        w="325px"
+        h="464px"
+        borderRadius="10px"
+        boxShadow="0 0px 10px 0 #1A1A1A10"
+        p="20px"
+      >
+        <Flex>
+          <Heading variant="titlesmall">incourse.run</Heading>
+        </Flex>
+      </Box>
     </Box>
   );
 };
