bfd83611e6d6fae4a3b8a1de3d3b7d8d3542ef6e
ChyunKim <co1995@naver.com>
2022-09-26 17:16:17 +0900

    feat(Contetn): Sperate content 2 on Main-Page #4

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import { BoxProps, Container } from '@chakra-ui/react';
 import HomeMain from '@components/common/@Layout/HomeLayout/_fragments/HomeMain';
 
 import FirstContent from './_fragments/FirstContent';
+import SecondContent from './_fragments/SecondContent';
 
 interface HomePageContentProps extends BoxProps {}
 
@@ -12,6 +13,7 @@ function HomePageContent({ ...basisProps }: HomePageContentProps) {
   return (
     <Container p="0" {...basisProps}>
       <FirstContent />
+      <SecondContent />
       <HomeMain />
     </Container>
   );
---
+++ src/components/HomePage/_fragments/SecondContent.tsx
@@ -0,0 +1,51 @@
+import { MY_IMAGES } from '@image';
+
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
+
+import { PlusIcon } from 'generated/icons/CustomIcon';
+
+const SecondContent = () => {
+  return (
+    <Box w="100%" h="782px" bg="#FFFCEF" border="none">
+      <Image
+        src={MY_IMAGES.IMAGES.PACK1.src}
+        alt={MY_IMAGES.IMAGES.PACK1.alt}
+        pt="20px"
+        pl="30px"
+      />
+      <Image
+        src={MY_IMAGES.IMAGES.PACK2.src}
+        alt={MY_IMAGES.IMAGES.PACK2.alt}
+        mt="-20px"
+        ml="139px"
+      />
+      <Image
+        src={MY_IMAGES.IMAGES.PACK3.src}
+        alt={MY_IMAGES.IMAGES.PACK3.alt}
+        mt="-40px"
+      />
+      <Flex>
+        <PlusIcon color="primary.500" mt="50px" ml="90px" mr="16px" />
+        <Heading variant="extraTitle" mt="-30px">
+          불합리한 유통구조
+          <br />
+          과도한 패키징
+          <br />
+          과장된 광고
+        </Heading>
+      </Flex>
+      <Box fontSize="20px" mt="30px" ml="75px">
+        <Text>부풀려지는 가격은 이제 그만!</Text>
+        <Flex>
+          <Text color="primary.500" fontWeight="700">
+            인코스런
+          </Text>
+          <Text>은 가격거품을 제거한</Text>
+        </Flex>
+        <Text>착한소비를 위해 태어났습니다.</Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default SecondContent;
