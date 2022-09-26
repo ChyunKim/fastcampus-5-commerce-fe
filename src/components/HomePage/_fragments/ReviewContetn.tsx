1bb45f7ca1860b62aa496f69e69c6cd5ff1c4e54
ChyunKim <co1995@naver.com>
2022-09-26 19:22:31 +0900

    feat: Publishing Review content #4

--- src/components/HomePage/_fragments/ReviewContetn.tsx
+++ src/components/HomePage/_fragments/ReviewContetn.tsx
@@ -1,4 +1,6 @@
-import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react';
+
+import { CallIcon, RadiostarIcon } from 'generated/icons/CustomIcon';
 
 const ReviewContent = () => {
   return (
@@ -60,11 +62,49 @@ const ReviewContent = () => {
         borderRadius="10px"
         boxShadow="0 0px 10px 0 #1A1A1A10"
         p="20px"
+        textAlign="left"
       >
-        <Flex>
+        <Flex alignItems="center" justifyContent="space-between">
           <Heading variant="titlesmall">incourse.run</Heading>
+          <Flex justifyContent="center">
+            <RadiostarIcon boxSize={3} color="primary.500" />
+            <RadiostarIcon boxSize={3} color="primary.500" />
+            <RadiostarIcon boxSize={3} color="primary.500" />
+            <RadiostarIcon boxSize={3} color="primary.500" />
+            <RadiostarIcon boxSize={3} color="gray.400" />
+          </Flex>
+        </Flex>
+        <Text color="gray.700" fontSize="12px">
+          2021.03.29
+        </Text>
+        <Text
+          w="auto"
+          h="225px"
+          fontSize="16px"
+          mt="30px"
+          textOverflow="ellipsis"
+          lineHeight="28px"
+        >
+          순해서 아이피부에도 자극없이 사용할 수 있어요! 아이 뿐 만아니라 온
+          가족이 사용할 수 있는 화장품이라고 추천받았어요. 처음엔 반신반의하는
+          마음으로 사용하기 시작했는데 지금은 모든 단계에서 인코스런 제품을
+          사용하고있어요! 아토피로 고생했던 우리 아이 피부도 지금은 거의
+          완치단계입니다 . 아이 엄마들에게 추천드려요!
+        </Text>
+        <Box
+          h="0"
+          border="1px solid"
+          borderColor="gray.300"
+          m="20px 0"
+          bg="gray.300"
+        />
+        <Flex justifyContent="space-between">
+          <Box borderRadius="5px" w="80px" h="80px" />
+          <Box borderRadius="5px" w="80px" h="80px" />
+          <Box borderRadius="5px" w="80px" h="80px" />
         </Flex>
       </Box>
+      <CallIcon w="50px" h="50px" float="right" mt="30px" />
     </Box>
   );
 };
