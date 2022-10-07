f33908ebcc603c22e830bf16e3135ac9d523299a
ChyunKim <co1995@naver.com>
2022-10-07 15:46:15 +0900

    feat: 카카오 로그인 연동

--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,5 +1,5 @@
 export const SOCIAL = {
-  KAKAO_CLIENT_ID: '',
+  KAKAO_CLIENT_ID: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
   NAVER_CLIENT_ID: '',
   FACEBOOK_CLIENT_ID: '',
   GOOGLE_CLIENT_ID: '',
