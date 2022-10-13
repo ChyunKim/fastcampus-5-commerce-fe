3fdd9b447f494b68ac702059309c003009b728df
ChyunKim <co1995@naver.com>
2022-10-13 20:51:30 +0900

    chore: social login 토큰 쿠키 저장 삭제

--- src/components/SocialLoginCallback/SocailLoginCallback.tsx
+++ src/components/SocialLoginCallback/SocailLoginCallback.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
 
 import { usePostSocialLoginMutation } from '@apis/user/UserApi.mutation';
 
@@ -8,17 +7,16 @@ import { ROUTES } from '@constants/routes';
 
 const SocialLoginCallback = () => {
   const router = useRouter();
-
-  const [cookies, setCookie] = useCookies(['socialToken']);
   const { mutateAsync: socialLogin } = usePostSocialLoginMutation();
   const { code, state } = router.query;
+
   useEffect(() => {
     if (typeof code === 'string' && typeof state === 'string') {
       socialLogin({ code: code, state: state })
         .then((res) => {
           if (res.isRegister) router.push(ROUTES.HOME);
           else {
-            setCookie('socialToken', res.socialToken);
+            console.log(res.socialToken);
             router.push(`${ROUTES.LOGIN.SIGNUP}`);
           }
         })
